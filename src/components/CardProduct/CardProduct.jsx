1b4644df402d6f88620886146e8c540ed408655c
WilmerProfesor <wilmerprofesor@gmail.com>
2023-12-27 20:58:21 -0500

    finalizado según requerimintos

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -3,19 +3,20 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import "./CardProduct.css";
 
 const CardProducto = ({ char }) => {
   const { image, title, price, description} = char;
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia component="img" image={image} alt="green iguana" sx={{ borderColor: "red" }} />
+    <Card sx={{ maxWidth: 345, marginTop: "20px" }}>
+      <CardActionArea className="CardArea">
+        <CardMedia component="img" image={image} alt="green iguana" sx={{ borderColor: "red" }} className="CardProductImg"/>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {price}
+            <strong className="Price">USD${price}</strong>
           </Typography>
           <Typography variant="body2" color="text.secondary">
             {description}
